5ab70129a67d45fefbc894050d251402ebd3bf0b
WakkaTrout <36018858+WakkaTrout@users.noreply.github.com>
2025-08-15 18:47:38 -0700

    Update cperfecthash.c
    Update notes from recent brute force tests

--- cperfecthash.c
+++ cperfecthash.c
@@ -1,9 +1,10 @@
 // FNV1 Hash (https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function)
-// NOTE: There are no 8-bit FNV1 Perfect hashes for the C programming language
-// NOTE: There are no 16-bit FNV1 Perfect hashes for the C programming language
+// NOTE: There are no 8-bit FNV1 Perfect hash functions for the C programming language
+// NOTE: There are no 16-bit FNV1 Perfect hash functions for the C programming language
 
 // FNV1A Hash (https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function)
-// NOTE: There are no 8-bit FNV1A Perfect hashes for the C programming language
+// NOTE: There are no 8-bit FNV1A Perfect hash functions for the C programming language
+// NOTE: There are no 16-bit FNV1A Perfect hash functions for the C programming language
 
 uint32_t c_perfect_hash_fnv1a(const char* string)
 {
