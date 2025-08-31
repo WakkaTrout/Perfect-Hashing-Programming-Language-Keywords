6fc3d216e05d84f3fa304f94dedeaf14124289cb
WakkaTrout <36018858+WakkaTrout@users.noreply.github.com>
2025-08-30 19:40:04 -0700

    Update cperfecthash.c
    Added a perfect hash function to the C perfect hashes

--- cperfecthash.c
+++ cperfecthash.c
@@ -1,11 +1,25 @@
 // FNV1 Hash (https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function)
-// NOTE: There are no 8-bit FNV1 Perfect hash functions for the C programming language
-// NOTE: There are no 16-bit FNV1 Perfect hash functions for the C programming language
+// NOTE: There are no 8-bit FNV1 Level 1 or higher Perfect hash functions for the C programming language
+// NOTE: There are no 16-bit FNV1 Level 1 or higher Perfect hash functions for the C programming language
+
+// Level 0 
+uint32_t c_perfect_hash_fnv1_with_mod(const char* string, const size_t string_len)
+{
+    uint32_t hash = 3; // fnv1 offset
+    for(size_t i = 0; i < string_len; ++i)
+    {
+        hash = hash * 3462989662; // fnv1 prime
+        hash = hash ^ ((uint32_t)string[i]);
+    }
+    hash = hash % 37; // mod to fit into 37 entries (there are 34 for C keywords)
+    return hash;
+}
 
 // FNV1A Hash (https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function)
 // NOTE: There are no 8-bit FNV1A Perfect hash functions for the C programming language
 // NOTE: There are no 16-bit FNV1A Perfect hash functions for the C programming language
 
+// Level 0 
 uint32_t c_perfect_hash_fnv1a(const char* string)
 {
   return 0;
