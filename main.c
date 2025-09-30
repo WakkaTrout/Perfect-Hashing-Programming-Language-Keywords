ba869e33e3819ca14a326f35cabbc35390f2b0d7
WakkaTrout <36018858+WakkaTrout@users.noreply.github.com>
2025-09-30 07:37:13 -0700

    Update main.c

--- main.c
+++ main.c
@@ -4,5 +4,14 @@
 
 int main(void)
 {
+  // Test 1:
+  // TODO: Load in a C program text file (the whole thing)
+  // TODO: Parse it using the perfect hash functions at various levels (so parse it repeadetly)
+  // TODO: Compare the results (time them)
+
+  // Test 2:
+  // TODO: Stream in a C program text file
+  // TODO: While streaming, parse it using the perfect hash function
+  // TODO: Compare the results
   return 0;
 }
